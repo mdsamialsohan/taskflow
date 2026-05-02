138ce2e153006c0a986bc1061aa0a34f74e128a3
samialsohan <samialsohan@gmail.com>
2026-05-02 20:38:37 +0100

    cli impoved

--- src/test/java/com/samialsohan/taskflow/BaseIntegrationTest.java
+++ src/test/java/com/samialsohan/taskflow/BaseIntegrationTest.java
@@ -1,24 +1,25 @@
 package com.samialsohan.taskflow;
 
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 
-@Testcontainers
+import org.springframework.context.annotation.Import;
+
+@Import(BaseIntegrationTest.TestcontainersConfiguration.class)
 public abstract class BaseIntegrationTest {
 
-    @Container
-    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:16-alpine")
-            .withDatabaseName("taskflow_test")
-            .withUsername("test")
-            .withPassword("test");
+    @TestConfiguration(proxyBeanMethods = false)
+    static class TestcontainersConfiguration {
 
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
+        @Bean
+        @ServiceConnection
+        PostgreSQLContainer postgresContainer() {
+            return new PostgreSQLContainer("postgres:16-alpine")
+                    .withDatabaseName("taskflow_test")
+                    .withUsername("test")
+                    .withPassword("test");
+        }
     }
 }
